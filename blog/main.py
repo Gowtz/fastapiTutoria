09026d914c74f0d8f829af9aaf7e819f607de032
gowtham <gowtham.v@games24x7.com>
2025-08-29 01:38:42 +0530

    Added delete and update method. updated the error response

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends,status,Response
+from fastapi import FastAPI,Depends,status,Response,HTTPException
 from typing import Optional
 from . import schema,models
 from .database import engine,SessionLocal
@@ -21,11 +21,7 @@ def get_db():
 @app.get('/posts')
 def getAllPost(response:Response,limit:Optional[int]=10,offset:Optional[int]= 0 ,db:Session = Depends(get_db)):
     if(limit >= 50):
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return {
-            "msg":"The limit is too high. Please provide a limit less than 50"
-        }
-    
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,detail="The limit is too high. Please provide a limit less than 50")
     data = db.query(models.Blog).offset(offset).limit(limit).all()
     return data
 
@@ -34,10 +30,7 @@ def getAllPost(id:int,response:Response,db:Session = Depends(get_db)):
     data = db.query(models.Blog).filter(models.Blog.id == id ).first()
     if not data:
         # Custom error response
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return{
-            "msg":"No Post Found"
-        }
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No Post Found")
     return data
         
 # Custom Status Code
@@ -48,3 +41,21 @@ def createPost(post:schema.Blog ,db: Session = Depends(get_db)): # Db Connect
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.delete('/post/{id}')
+def deletePost(id:int,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete()
+    db.commit()
+    return {"msg":f"the blog with the id {id} has been deleted"}
+
+@app.put('/post/{id}')
+def updatePost(id:int,post:schema.Blog,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status=status.HTTP_404_NOT_FOUND,detail=f"THe Blog with the id {id} not found")
+    blog.update({"title":post.title,'content':post.content,'author':post.author})
+    db.commit()
+    return {
+        "msg":"Done",
+        "data":blog
+    }
